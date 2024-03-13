b19629084ce4c466a8fddd625890994934f5b573
sdn <cdna.simon@gmail.com>
2024-03-13 17:24:56 +0100

    add keymaps for gitsigns

--- .config/nvim/lua/simoncdn/plugins/gitsigns.lua
+++ .config/nvim/lua/simoncdn/plugins/gitsigns.lua
@@ -1,15 +1,20 @@
 return {
-	 -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gsh", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gsl", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
