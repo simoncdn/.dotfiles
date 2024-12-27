41c8df4410ea49dbe2cd9b5f3e5270e6a7d3793f
Simon Cardona <cdna.simon@gmail.com>
2024-12-27 10:35:15 +0100

    feat(kitty): add Dank Mono font

--- .config/nvim/lua/simoncdn/plugins/colors.lua
+++ .config/nvim/lua/simoncdn/plugins/colors.lua
@@ -22,7 +22,9 @@ return {
 					-- Style to be applied to different syntax groups
 					-- Value is any valid attr-list value for `:help nvim_set_hl`
 					comments = { italic = false },
-					keywords = { italic = false },
+					keywords = { italic = true },
+					functions = { italic = true },
+					variables = { italic = false },
 					-- Background styles. Can be "dark", "transparent" or "normal"
 					sidebars = "transparent", -- style for sidebars, see below
 					floats = "transparent", -- style for floating windows
